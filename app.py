209201ff6e5ea1abdd7abb43e510e2e9210c07fa
Oluwaseun Alagbe <Oluseunalagbee@gmail.com>
2022-07-12 15:20:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def upload():
     #this section is used by gunicorn to serve the app on Heroku
 if __name__ == '__main__':
 
-        app.run()
+        app.run(host='0.0.0.0')
     #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
     # Serve the app with gevent 
         #http_server = WSGIServer(('', 5000), app)
